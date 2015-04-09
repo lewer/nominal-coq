554c51fd2f6fba013d68f94017c746b412e263fc
Gabriel Lewertowski <gabriel.lewertowski@polytechnique.edu>
2015-04-09 16:21:31 +0200

    ajout de la propriété stable dans le Record finPerm, preuve de perm_inj

--- theories/quotlambda_new.v
+++ theories/quotlambda_new.v
@@ -11,6 +11,7 @@ Definition atom := nat.
 
 Record finPerm := FinPerm {
   perm_of : finsfun (@id atom);
+  stable (a:atom) (kf: a \in domf (finsfun_of perm_of)): (finsfun_of perm_of).[kf]%fmap \in domf (finsfun_of perm_of);
   inj : injectiveb (finsfun_of perm_of)
 }.
 
@@ -25,8 +26,12 @@ Definition support (π : finPerm) :=
  a : Key.sort nat_KeyType ? *)
 
 
-Lemma in_support π a: a \in (support π) -> π a != a.
-Proof. by move => kf; rewrite/fun_of_perm /fun_of_finsfun in_fnd can. Qed.
+Lemma in_support π a: a \in (support π) <-> (π a != a).
+Proof. 
+split.
+  by move => kf; rewrite/fun_of_perm /fun_of_finsfun in_fnd can. 
+  by apply:contraR => H; rewrite /fun_of_perm /fun_of_finsfun not_fnd.
+Qed.
 
 (* Ici je donne le nom "can" à l'hypothèse qui dit que 
 les finsfun sont canoniques, ou "inj" à l'hypothèse qui 
@@ -35,11 +40,14 @@ dans la définition de finPerm inj par _, comment est-ce
 que j'y fais référence dans les preuves ? *) 
 
 Lemma perm_default π a: a\notin (support π) -> π a = a.
-Proof. by move => H; rewrite/fun_of_perm /fun_of_finsfun not_fnd //=. Qed.
+Proof. by move => H; rewrite /fun_of_perm /fun_of_finsfun not_fnd. Qed.
 
 (* Ici est-ce qu'il vaut mieux conclure π a = a ou π a == a ?
 Le premier permet des rewrite, mais le deuxième est
-plus précis... *) 
+plus précis...
+Et si je veux écrire une équivalence entre a\notin (support π) et π a = a,
+c'est quoi la meilleure façon de l'écrire ?
+ *) 
 
 CoInductive perm_spec (π : finPerm) : atom -> bool -> atom -> Type :=
   | PermOut a : a \notin support π -> perm_spec π a false a
@@ -49,9 +57,9 @@ CoInductive perm_spec (π : finPerm) : atom -> bool -> atom -> Type :=
 
 Lemma permP (π : finPerm) (a : atom) : perm_spec π a (a \in support π) (π a).
 Proof.
-case_eq (a \in support π) => H. 
-by rewrite /fun_of_perm /fun_of_finsfun in_fnd; apply:PermIn.
-  rewrite perm_default; last by apply:negbT.
+case_eq (a \in support π); move => H.
+  by rewrite /fun_of_perm /fun_of_finsfun in_fnd; apply:PermIn.
+  rewrite (perm_default); last by apply:negbT.
   by apply PermOut; apply negbT. 
 Qed.
 
@@ -70,15 +78,11 @@ Lemma perm_inj (π : finPerm) : injective π.
 Proof.
 move => a1 a2.
 case:permP; case:permP => {a1} {a2} a1 Ha1 a2 Ha2; first by [].
-  move -> . (* contradictoire avec can *)
- 
-(* IN PROGRESS *)
-
-Lemma in_ssupport (π : finPerm) : {mono π : a / a \in support π}.
-Proof.
-move => a /=. case:permP; first by move => ? /negPf.
-move => {a} a H. case:fndP. done. move => H2. 
-move/perm_default:(H2)=>H3. 
-
-have: π a = a. move: (inj π).  move /injectiveP. Check perm_of π.
-rewrite /injective. move:H. rewrite/support/domf.
+  -have : (finsfun_of (perm_of π)) {| ssval := a1; ssvalP := Ha1 |} \in support π
+    by rewrite /support; apply stable.
+  move => H1 H2; move : H2 H1 <- =>  H. admit.
+  -have : (finsfun_of (perm_of π)) {| ssval := a2; ssvalP := Ha2 |} \in support π
+    by rewrite /support; apply stable.                                                             move => H1 H2; move : H2 H1 <- => H. admit.   
+  - move : (inj π) => /injectiveP π_inj /eqP. rewrite (inj_eq π_inj).
+    by move => ?; apply/eqP.
+Qed.
